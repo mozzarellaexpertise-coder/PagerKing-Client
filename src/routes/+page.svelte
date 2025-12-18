d65392e1edd6d8751e2676db97822792a32d58aa
GAT <mozzarellaexpertise@gmail.com>
2025-12-18 14:36:02 +0630

    :ROOT!!!!

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    goto('/login');
+  });
+</script>
+
+<h1>PagerKing</h1>
+<p>Redirecting…</p>
