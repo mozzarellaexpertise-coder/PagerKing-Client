5ff245f8b25695d102e4b52e9c6d295e80b69e9a
GAT <mozzarellaexpertise@gmail.com>
2025-12-20 00:22:30 +0630

    :SLASHyyyy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<script>
+<script lang="ts">
+  import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
-  goto('/login');
+  import { browser } from '$app/environment';
+
+  // WRONG: This runs on the server and causes the 500 crash
+  // goto('/login'); 
+
+  // RIGHT: This only runs once the page is in the browser
+  onMount(() => {
+    goto('/login');
+  });
+
+  // OR USE AN IF-CHECK:
+  // if (browser) {
+  //   goto('/login');
+  // }
 </script>
